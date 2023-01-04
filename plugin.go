13f139d0a65cc6876c475a2069dc7208529b751d
Jason Chu <me@chuie.io>
2023-01-04 01:55:36 -0800

    maybe set defaults

--- plugin.go
+++ plugin.go
@@ -8,28 +8,19 @@ import (
 )
 
 // Config the plugin configuration.
-type Config struct {
-	PublicKey             string                           `json:"publicKey"`
-	Token                 string                           `json:"secret"`
-	AllowedUsers          []string                         `json:"allowedUsers"`
-	IDFormats             []string                         `json:"idFormats"`
-	Except                []string                         `json:"except"`
-	Include               []string                         `json:"include"`
-	Headers               map[string]*tmpauth.HeaderOption `json:"headers"`
-	Redirect              string                           `json:"redirect"`
-	Host                  string                           `json:"host"`
-	Debug                 bool                             `json:"debug"`
-	CaseSensitiveMatching bool                             `json:"caseSensitiveMatching"`
-}
+type Config = tmpauth.UnserializableConfig
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
-	return new(Config)
+	return &Config{
+		PublicKey: "BN/PHEYgs0meH878gqpWl81WD3zEJ+ubih3RVYwFxaYXxHF+5tgDaJ/M++CRjur8vtXxoJnPETM8WRIc3CO0LyM=",
+		Token:     "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoLnRtcGltLnB3OnNlcnZlcjprZXk6ZmUyNzhhMmIwODY1NWM5YjYzNjQzZjI0N2E3NGFkMDciLCJpc3MiOiJhdXRoLnRtcGltLnB3OmNlbnRyYWwiLCJzZWNyZXQiOiJ6d2Q5TUpDVy9CbWdBcjNjeE0wbE1CU2tkaVVUa0JhUmNXZFp3ZkJPeGZRPSIsImlhdCI6MTY3MjczNTAxMSwic3ViIjoiZmUyNzhhMmIwODY1NWM5YjYzNjQzZjI0N2E3NGFkMDcifQ.RIpQebD1IgC7m7vtLzp_dDxNN0y6WSpU68PxlNBL9Ru7eFiP7hAbUwxX8X7B0PJv1MuRbJxrXpa2cwVChwIZfA",
+	}
 }
 
 // New creates a new tmpauth plugin instance.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	parsedCfg, err := (*tmpauth.UnserializableConfig)(config).Parse()
+	parsedCfg, err := config.Parse()
 	if err != nil {
 		return nil, err
 	}
